7f7d090656308831a288880bc50fcbaada30f5ce
Shammah Chancellor <shammah.chancellor@gmail.com>
2019-04-11 13:13:57 -0700

    [reloader] Add support for multiple config files
    Currently the config file expects there to only be one config file
    watched.  However, the nats config file can include other paths, and
    we may also want to watch those.
    
    This commit implements being able to specify an arbtirary number of
    configuration files to be watched.

--- cmd/reloader/main.go
+++ cmd/reloader/main.go
@@ -7,11 +7,25 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
-	"github.com/nats-io/nats-operator/pkg/reloader"
+	natsreloader "github.com/nats-io/nats-operator/pkg/reloader"
 	"github.com/nats-io/nats-operator/version"
 )
+
+// StringSet is a wrapper for []string to allow using it with the flags package.
+type StringSet []string
+
+func (s StringSet) String() string {
+	return strings.Join([]string(s), ", ")
+}
+
+// Set appends the value provided to the list of strings.
+func (s StringSet) Set(val string) error {
+	s = append(s, val)
+	return nil
+}
 
 func main() {
 	fs := flag.NewFlagSet("nats-server-config-reloader", flag.ExitOnError)
@@ -34,12 +48,16 @@ func main() {
 	nconfig := &natsreloader.Config{}
 	fs.StringVar(&nconfig.PidFile, "P", "/var/run/nats/gnatsd.pid", "NATS Server Pid File")
 	fs.StringVar(&nconfig.PidFile, "pid", "/var/run/nats/gnatsd.pid", "NATS Server Pid File")
-	fs.StringVar(&nconfig.ConfigFile, "c", "/etc/nats/gnatsd.conf", "NATS Server Config File")
-	fs.StringVar(&nconfig.ConfigFile, "config", "/etc/nats/gnatsd.conf", "NATS Server Config File")
+	fs.Var(StringSet(nconfig.ConfigFiles), "c", "NATS Server Config File")
+	fs.Var(StringSet(nconfig.ConfigFiles), "config", "NATS Server Config File")
 	fs.IntVar(&nconfig.MaxRetries, "max-retries", 5, "Max attempts to trigger reload")
 	fs.IntVar(&nconfig.RetryWaitSecs, "retry-wait-secs", 2, "Time to back off when reloading fails before retrying")
 
 	fs.Parse(os.Args[1:])
+
+	if len(nconfig.ConfigFiles) == 0 {
+		nconfig.ConfigFiles = []string{"/etc/nats/gnatsd.conf"}
+	}
 
 	switch {
 	case showHelp:
--- pkg/reloader/reloader.go
+++ pkg/reloader/reloader.go
@@ -9,7 +9,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"path"
+	"path/filepath"
 	"strconv"
 	"syscall"
 	"time"
@@ -20,7 +20,7 @@ import (
 // Config represents the configuration of the reloader.
 type Config struct {
 	PidFile       string
-	ConfigFile    string
+	ConfigFiles   []string
 	MaxRetries    int
 	RetryWaitSecs int
 }
@@ -39,10 +39,6 @@ type Reloader struct {
 
 	// quit shutsdown the reloader.
 	quit func()
-
-	// lastAppliedVersion is the last config update
-	// done by the proces..
-	lastAppliedVersion []byte
 }
 
 // Run starts the main loop.
@@ -94,11 +90,20 @@ func (r *Reloader) Run(ctx context.Context) error {
 	// Follow configuration updates in the directory where
 	// the config file is located and trigger reload when
 	// it is either recreated or written into.
-	if err := configWatcher.Add(path.Dir(r.ConfigFile)); err != nil {
-		return err
+	for i := range r.ConfigFiles {
+		// Ensure our paths are canonical
+		r.ConfigFiles[i], _ = filepath.Abs(r.ConfigFiles[i])
+		if err := configWatcher.Add(filepath.Dir(r.ConfigFiles[i])); err != nil {
+			return err
+		}
 	}
 
 	attempts = 0
+	// lastConfigAppliedCache is the last config update
+	// applied by us
+	lastConfigAppliedCache := make(map[string][]byte)
+
+WaitForEvent:
 	for {
 		select {
 		case <-ctx.Done():
@@ -110,25 +115,43 @@ func (r *Reloader) Run(ctx context.Context) error {
 			if event.Op != fsnotify.Write && event.Op != fsnotify.Create {
 				continue
 			}
-
-			h := sha256.New()
-			f, err := os.Open(r.ConfigFile)
+			touchedInfo, err := os.Stat(event.Name)
 			if err != nil {
-				log.Printf("Error: %s\n", err)
 				continue
 			}
-			if _, err := io.Copy(h, f); err != nil {
-				log.Printf("Error: %s\n", err)
-				continue
-			}
-			digest := h.Sum(nil)
-			if r.lastAppliedVersion != nil {
-				if bytes.Equal(r.lastAppliedVersion, digest) {
-					// Skip since no meaningful change
-					continue
+
+			for _, configFile := range r.ConfigFiles {
+				configInfo, err := os.Stat(configFile)
+				if err != nil {
+					log.Printf("Error: %s\n", err)
+					continue WaitForEvent
 				}
+				if !os.SameFile(touchedInfo, configInfo) {
+					continue WaitForEvent
+				}
+
+				h := sha256.New()
+				f, err := os.Open(configFile)
+				if err != nil {
+					log.Printf("Error: %s\n", err)
+					continue WaitForEvent
+				}
+				if _, err := io.Copy(h, f); err != nil {
+					log.Printf("Error: %s\n", err)
+					continue WaitForEvent
+				}
+				digest := h.Sum(nil)
+				lastConfigHash, ok := lastConfigAppliedCache[configFile]
+				if ok && bytes.Equal(lastConfigHash, digest) {
+					// No meaningful change
+					continue WaitForEvent
+				}
+				lastConfigAppliedCache[configFile] = digest
+
+				// We only get an event for one file at a time, we can stop checking
+				// config files here and continue with our business.
+				break
 			}
-			r.lastAppliedVersion = digest
 
 		case err := <-configWatcher.Errors:
 			log.Printf("Error: %s\n", err)
